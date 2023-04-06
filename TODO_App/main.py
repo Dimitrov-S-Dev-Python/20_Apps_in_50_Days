0ec6a7b2e2e059630f9983f533c2139c197061da
Stan <dimitrov.s.dev@gmail.com>
2023-04-06 06:56:38 +0300

    20_Apps_in_50_Days

--- TODO_App/main.py
+++ TODO_App/main.py
@@ -4,16 +4,20 @@ while True:
     match user_action:
         case "add":
             todo = input("Enter a todo: ") + "\n"
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
             todos.append(todo)
 
-            file = open("todos.txt", "w")
-            file.writelines(todos)
-            file.close()
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+
         case "show":
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+
             for index, item in enumerate(todos):
+                item = item.strip("\n")
                 print(f"{index + 1}.{item.capitalize()}")
 
         case "edit":
