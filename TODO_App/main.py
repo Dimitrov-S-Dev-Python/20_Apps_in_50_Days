8c432f15f3c7d7ce40762f509e798f5ec6508665
Stan <dimitrov.s.dev@gmail.com>
2023-04-07 02:01:16 +0300

    20_Apps_in_50_Days

--- TODO_App/main.py
+++ TODO_App/main.py
@@ -1,4 +1,4 @@
-from functions import get_todos, write_todos
+import functions
 
 while True:
     user_action = input("Type add, show, complete edit or exit: ")
@@ -7,15 +7,15 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + "\n")
 
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip("\n")
@@ -25,11 +25,11 @@ while True:
         try:
             number = int(user_action[5:])
             index = number - 1
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter the new todo: ")
             todos[index] = new_todo + "\n"
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Your command is not valid.")
             continue
@@ -38,11 +38,11 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip("\n")
             todos.pop(index)
-            write_todos(todos)
+            functions.write_todos(todos)
 
             print(f"Todo {todo_to_remove} was removed from the list.")
 
