ab11381a114e4084dabdf6dcbf5b31b587e99201
Stan <dimitrov.s.dev@gmail.com>
2023-04-06 05:17:13 +0300

    20_Apps_in_50_Days

--- TODO_App/main.py
+++ TODO_App/main.py
@@ -1,21 +1,26 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, edit or exit: ")
+    user_action = input("Type add, show, complete edit or exit: ")
     user_action = user_action.strip()
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            for item in todos:
-                print(item)
+            for index, item in enumerate(todos):
+                print(f"{index + 1}.{item}")
 
         case "edit":
             number = int(input("Number of todo to edit: "))
             index = number - 1
             new_todo = input("Enter the new todo: ")
             todos[index] = new_todo
+
+        case "complete":
+            number = int(input("Number of todo to complete: "))
+            todos.pop(number - 1)
+
         case "exit":
             break
 
