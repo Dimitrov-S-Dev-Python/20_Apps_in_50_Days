d0253573ad9c0364168e49ca4dab1aaf7aa3bc3e
Stan <dimitrov.s.dev@gmail.com>
2023-04-07 03:58:22 +0300

    20_Apps_in_50_Days

--- BONUS/main.py
+++ BONUS/main.py
@@ -5,13 +5,20 @@ with open("data.json", "r") as file:
 
 data = json.loads(content)
 
+score = 0
 for question in data:
     print(question["question_text"])
     for index, value in enumerate(question["options"]):
         print(f"{index + 1}. {value}")
-    user_choice = int(input("Enter your data"))
+    user_choice = int(input("Enter your number guess: "))
     question["user_choice"] = user_choice
+    if question["user_choice"] == question["correct_answer"]:
+        score += 1
 
+for index, question in enumerate(data):
+    message = f"{index + 1}.Your answer: {question['user_choice']},\"" \
+              f"Correct answer: {question['correct_answer']}"
+    print(message)
 
-
+print(f"{score}/{len(data)}")
 
