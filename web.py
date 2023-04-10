e7d93f8123ad74cdcf5986da3fd91d6ee0cb86b9
Stan <dimitrov.s.dev@gmail.com>
2023-04-10 16:18:46 +0300

    20_Apps_in_50_Days

--- web.py
+++ web.py
@@ -12,10 +12,12 @@ def add_todo():
 
 st.title("My Todo App")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Enter a new todo...", on_change=add_todo, key="new_todo")
-
-print("hello")
-st.session_state
